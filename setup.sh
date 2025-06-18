33f5ba7f87cb100debe70d374204c1fd54dff98d
StevenBeach33 <107557824+StevenBeach33@users.noreply.github.com>
2025-06-18 17:31:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ pkg update && pkg upgrade -y
 pkg install file which python3 python-lxml curl wget zip -y
 
 #make it executable and move pkg2zip to system
-chmod a+x *
-chmod +x bin/pkg2zip
+chmod +x nps_*.sh pyNPU.py bin/pkg2zip
+test -d "$PREFIX/bin" && ln -s "$(pwd)"/nps_*.sh "$(pwd)"/pyNPU.py "$PREFIX/bin"
 mv bin/pkg2zip $PREFIX/bin
-bash nps_tsv.sh
+./nps_tsv.sh
 mkdir -p /sdcard/NPS/{PSV/{GAME,DLC,UPDATE},PSP/GAME,PSM/GAME}
